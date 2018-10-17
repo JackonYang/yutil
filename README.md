2fccebbd34a537edd3d2bef00a632b04e164ffcd
jackonyang <i@jackon.me>
2018-10-17 12:41:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# yutil
+
+my utils
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(name='yutil',
+      version='0.1',
+      description=('my utils'),
+      url='https://github.com/JackonYang/yutil',
+      author='Jackon Yang',
+      author_email='i@jackon.me',
+      license='MIT',
+      packages=['yutil'],
+      zip_safe=False)
