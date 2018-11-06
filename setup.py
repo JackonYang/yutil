998bd2de6bb43fdcd9d9398ea8f52069160e15f7
Jackon Yang <i@jackon.me>
2018-11-07 02:49:01 +0800

    setup env for text_cleaning

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 from setuptools import setup
 
+from pip.req import parse_requirements
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements-prod.txt')
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
 
 setup(name='yutil',
       version='0.1',
@@ -9,4 +19,5 @@ setup(name='yutil',
       author_email='i@jackon.me',
       license='MIT',
       packages=['yutil'],
+      install_requires=reqs,
       zip_safe=False)
